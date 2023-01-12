d99485b6553fd844f8849e7caaa4b2592e525a91
azu <azuciao@gmail.com>
2023-01-12 23:39:37 +0900

    add title

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
         <>
-            <title></title>
+            <title>mubook-hon</title>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
             <link rel="icon" href="/favicon.ico" />
             <link rel="manifest" href="/manifest.json" />
