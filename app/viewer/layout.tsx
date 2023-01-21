b889bc2b0ace177497bf138b044d3d452717b30e
azu <azuciao@gmail.com>
2023-01-21 19:46:43 +0900

    fix: use full-app class

--- app/viewer/layout.tsx
+++ app/viewer/layout.tsx
@@ -1,12 +1,11 @@
-import "./global.css";
-
+import "../global.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html>
             <head>
                 <title>mu-book-hon</title>
             </head>
-            <body>{children}</body>
+            <body className={"full-app"}>{children}</body>
         </html>
     );
 }
