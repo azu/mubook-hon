e947fcc43e48e4b75907f13933d771f017fc3d00
azu <azuciao@gmail.com>
2023-07-28 16:36:14 +0900

    fix use localStorage

--- app/settings/useUserSettings.ts
+++ app/settings/useUserSettings.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react";
+import { useLocalStorage } from "react-use";
 
 type UserSettings = {
     openNewTab: boolean;
@@ -7,23 +7,9 @@ const DEFAULT_SETTINGS: UserSettings = {
     openNewTab: true
 };
 export const useUserSettings = () => {
-    const [memoryState, setMemoryState] = useState<UserSettings>(DEFAULT_SETTINGS);
-    const userSettings = useMemo(() => {
-        const settings = localStorage.getItem("mubook-hon.settings");
-        if (settings) {
-            return {
-                ...memoryState,
-                ...JSON.parse(settings)
-            } as UserSettings;
-        }
-        return DEFAULT_SETTINGS;
-    }, [memoryState]);
-    const updateUserSettings = useCallback((settings: UserSettings) => {
-        setMemoryState(settings);
-        localStorage.setItem("mubook-hon.settings", JSON.stringify(settings));
-    }, []);
+    const [userSettings, setUserSettings] = useLocalStorage<UserSettings>("mubook-hon-user-settings", DEFAULT_SETTINGS);
     return {
-        userSettings,
-        updateUserSettings
+        userSettings: userSettings,
+        updateUserSettings: setUserSettings
     } as const;
 };
